02f7ffb36601291941259ee42e0f8cd0f49f8b54
MatosRicardo <ricardoribeirodematos@gmail.com>
2025-02-06 19:33:18 -0300

    feat: styles e states

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
+import { useState } from "react";
 import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx"; //Importanto componente externo
 import CoreConcept from "./components/CoreConcept.jsx"; //Importanto props
+import TabButton from "./components/TabButton.jsx";
+import { EXAMPLES } from "./data.js";
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  //FUNÇÃO DE EVENTO
+  function handleSelect(selectButton) {
+    setSelectedTopic(selectButton);
+  }
+
+  const tabContent = <p>Selecione um tópico.</p>;
+
+  //Maneira de usar usando o IF
+
+  // if (selectedTopic) {
+  //   tabContent = (
+  //     <div id="tab-content">
+  //       <h3>{EXAMPLES[selectedTopic].title}</h3>
+  //       <p>{EXAMPLES[selectedTopic].description}</p>
+  //       <pre>
+  //         <code>{EXAMPLES[selectedTopic].code}</code>
+  //       </pre>
+  //     </div>
+  //   );
+  // }
+
   return (
     <div>
       <Header />
@@ -29,13 +55,30 @@ function App() {
         <section id="examples">
           <h2>Exemplos</h2>
           <menu>
-            <li>
-              <button></button>
-            </li>
-            <li>
-              <button></button>
-            </li>
+            {/* Ação de evento */}
+            <TabButton onSelect={() => handleSelect("components")}>
+              Componentes
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
+
+          {/* Maneira menor de fazer o evento, usando o if começo */}
+          {/* <tabContent /> */}
+          {/* fim  */}
+
+          {!selectedTopic && <p>Selecione um tópico.</p>}
+          {/* Ação de Evento */}
+          {selectedTopic ? (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          ) : null}
         </section>
       </main>
     </div>
---
+++ src/components/TabButton.jsx
@@ -0,0 +1,10 @@
+export default function TabButton({ children, onSelect }) {
+  return (
+    <li>
+      <button className="active" onClick={onSelect}>
+        {children}
+      </button>
+    </li>
+  );
+}
+//Ação de Evento
